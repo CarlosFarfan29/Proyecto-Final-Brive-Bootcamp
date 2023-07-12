375123cd0d966d5cf52296579caa02f4c8f7e61d
karla98 <riosdiazkm@gmail.com>
2023-07-12 01:26:52 -0600

    estilos home occ

--- src/components/OCC/OrdenamientoRegistros.jsx
+++ src/components/OCC/OrdenamientoRegistros.jsx
@@ -9,13 +9,35 @@ export function OrdenamientoRegistros() {
     { id: 1, totalVacantes: 10, nombreEmpresa: "Famsa", fecha: "2022-07-22" },
     { id: 2, totalVacantes: 50, nombreEmpresa: "Walmart", fecha: "2022-02-01" },
     { id: 3, totalVacantes: 29, nombreEmpresa: "Brive", fecha: "2022-03-05" },
-    { id: 4, totalVacantes: 81, nombreEmpresa: "OCCMundial", fecha: "2022-03-01"},
+    {
+      id: 4,
+      totalVacantes: 81,
+      nombreEmpresa: "OCCMundial",
+      fecha: "2022-03-01",
+    },
     { id: 5, totalVacantes: 2, nombreEmpresa: "RopaShop", fecha: "2022-05-01" },
     { id: 6, totalVacantes: 22, nombreEmpresa: "Empresa", fecha: "2022-06-01" },
-    { id: 7, totalVacantes: 66, nombreEmpresa: "Facebook", fecha: "2022-06-01"},
+    {
+      id: 7,
+      totalVacantes: 66,
+      nombreEmpresa: "Facebook",
+      fecha: "2022-06-01",
+    },
     { id: 8, totalVacantes: 89, nombreEmpresa: "Youtube", fecha: "2022-07-01" },
   ]);
-  
+
+  /*
+//empresa -> vacantantes[]
+// vacante->empresa;
+
+  const [registros, setRegistros] = useState([
+    { id: 1, nombre_empleo: 'nombre 1', sueldo: 12.00, fecha_publicacion: "2022-07-22", empresaId : 2, empresa:{id: 1, nombreEmpresa: "Famsa", ubicacionEmpresa: "León"} },
+    { id: 1, nombre_empleo: 'nombre 1', sueldo: 12.00, fecha_publicacion: "2022-07-22", empresaId : 2, empresa:{id: 1, nombreEmpresa: "Famsa", ubicacionEmpresa: "León"} },
+    { id: 1, nombre_empleo: 'nombre 1', sueldo: 12.00, fecha_publicacion: "2022-07-22", empresaId : 2, empresa:{id: 1, nombreEmpresa: "Famsa", ubicacionEmpresa: "León"} },
+    { id: 1, nombre_empleo: 'nombre 1', sueldo: 12.00, fecha_publicacion: "2022-07-22", empresaId : 2, empresa:{id: 1, nombreEmpresa: "Famsa", ubicacionEmpresa: "León"} },
+  ]);
+  */
+
   /* Inicio Logica Paginación */
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(4);
@@ -48,14 +70,15 @@ export function OrdenamientoRegistros() {
     let registrosOrdenados = [...registros]; // Esto hace una copia de los registros originales
     registrosOrdenados.sort((a, b) => {
       let comparacion = 0;
-  
+
       if (totalVacantes !== "") {
         const ordenTotalVacantes = totalVacantes === "asc" ? 1 : -1;
         if (a.totalVacantes !== b.totalVacantes) {
-          comparacion = (a.totalVacantes - b.totalVacantes) * ordenTotalVacantes;
+          comparacion =
+            (a.totalVacantes - b.totalVacantes) * ordenTotalVacantes;
         }
       }
-  
+
       if (nombreEmpresa !== "") {
         const ordenNombreEmpresa = nombreEmpresa === "asc" ? 1 : -1;
         if (a.nombreEmpresa !== b.nombreEmpresa) {
@@ -63,26 +86,24 @@ export function OrdenamientoRegistros() {
             a.nombreEmpresa.localeCompare(b.nombreEmpresa) * ordenNombreEmpresa;
         }
       }
-  
+
       if (fecha !== "") {
         const ordenFecha = fecha === "asc" ? 1 : -1;
         if (a.fecha !== b.fecha) {
           comparacion = (new Date(a.fecha) - new Date(b.fecha)) * ordenFecha;
         }
       }
-  
+
       return comparacion;
     });
-  
+
     setRegistros(registrosOrdenados);
   };
-
 
   // Llamar a handleSearch para ordenar los registros
   useEffect(() => {
     handleSearch();
   }, [totalVacantes, nombreEmpresa, fecha]); // dependencias necesarias para activar el ordenamiento
-
 
   return (
     <div className="flex flex-col">
@@ -129,42 +150,49 @@ export function OrdenamientoRegistros() {
           <option value="desc">desc</option>
         </select>
 
-
-      {/*
-        <button
-          onClick={handleSearch}
-          className="bg-terciary-color hover:bg-orange-color text-white px-4 py-2 rounded-md ml-4"
-        >
-          Ordenar
-        </button>
-      */}
+        {/* Botón de Ordenar */}
+        {/* Comentado temporalmente */}
+        {/* <button
+      onClick={handleSearch}
+      className="bg-terciary-color hover:bg-orange-color text-white px-4 py-2 rounded-md ml-4"
+    >
+      Ordenar
+    </button> */}
       </div>
 
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {currentItems.map((registro) => (
           <Empresas key={registro.id} registro={registro} />
         ))}
       </div>
 
-      <div className="pagination-occ">
-        <button
-          className="prev-button-occ"
-          onClick={() => handlePagina(currentPage-1)}
-          disabled={!(currentPage-1)}
-        >
-          {((currentPage-1) === 0) ? 'No hay página previa' : (currentPage-1)}
-        </button>
-        <p>
-          {currentPage}
-          de {totalPages}
-        </p>
-        <button
-          className="next-button-occ"
-          onClick={() => handlePagina(currentPage+1)}
-          disabled={(currentPage+1)>totalPages}
-        >
-          {((currentPage+1)>totalPages) ? 'No hay más páginas por mostrar' : (currentPage+1)}
-        </button>
+      <div className="flex justify-center mt-4">
+        <div className="pagination-occ">
+          {/* Botón de página anterior */}
+          <button
+            className="prev-button-occ"
+            onClick={() => handlePagina(currentPage - 1)}
+            disabled={currentPage - 1 <= 0}
+          >
+            {currentPage - 1 <= 0 ? "No hay página previa" : currentPage - 1}
+          </button>
+
+          {/* Número de página actual */}
+          <p>
+            {currentPage} de {totalPages}
+          </p>
+
+          {/* Botón de página siguiente */}
+          <button
+            className="next-button-occ"
+            onClick={() => handlePagina(currentPage + 1)}
+            disabled={currentPage + 1 > totalPages}
+          >
+            {currentPage + 1 > totalPages
+              ? "No hay más páginas por mostrar"
+              : currentPage + 1}
+          </button>
+        </div>
       </div>
     </div>
   );
