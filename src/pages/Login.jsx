31c6b65d39dcbb9a479f3d1e9f709d6decd17fc1
CarlosFarfan29 <cfarfana@unac.edu.pe>
2023-07-12 19:20:16 -0500

    fix login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Alerta from "../components/Alerta";
-import { AiOutlineEye, AiOutlineEyeInvisible, AiFillLock, AiFillMail} from "react-icons/ai";
+import { AiOutlineEye, AiOutlineEyeInvisible, AiFillLock, AiFillMail } from "react-icons/ai";
 import { useNavigate } from "react-router";
 
 const Login = () => {
@@ -10,10 +10,27 @@ const Login = () => {
   const [alerta, setAlerta] = useState({});
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
+    // Validar campos vacíos
+    if (email.trim() === "" || password.trim() === "") {
+      setAlerta({
+        msg: "Por favor, completa todos los campos",
+        error: true,
+      });
+      return;
+    }
+
+    // Validar el formato del correo electrónico
+    if (!isValidEmail(email)) {
+      setAlerta({
+        msg: "Por favor, ingresa un correo electrónico válido",
+        error: true,
+      });
+      return;
+    }
+
     try {
       const response = await fetch("https://localhost:7219/api/User/validar-credenciales", {
         method: "POST",
@@ -25,17 +42,31 @@ const Login = () => {
           password
         })
       });
-  
+
       if (response.ok) {
         // Inicio de sesión exitoso
         navigate('/home');
       } else {
-        // Error de inicio de sesión
         const data = await response.json();
-        setAlerta({
-          msg: data.errorMsg || "Error de inicio de sesión",
-          error: true,
-        });
+        if (response.status === 401) {
+          // Credenciales incorrectas
+          setAlerta({
+            msg: "Las credenciales ingresadas son incorrectas",
+            error: true,
+          });
+        } else if (response.status === 404) {
+          // Cuenta no existe
+          setAlerta({
+            msg: "La cuenta no existe. Por favor, verifica el correo electrónico ingresado",
+            error: true,
+          });
+        } else {
+          // Error de inicio de sesión
+          setAlerta({
+            msg: data.errorMsg || "Error de inicio de sesión",
+            error: true,
+          });
+        }
       }
     } catch (error) {
       // Error de red u otro error
@@ -46,10 +77,16 @@ const Login = () => {
       console.error(error);
     }
   };
-  
+
   function togglePasswordVisibility() {
     setIsPasswordVisible((prevState) => !prevState);
   }
+
+  const isValidEmail = (email) => {
+    // Expresión regular para validar el formato del correo electrónico
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return emailRegex.test(email);
+  };
 
   const { msg } = alerta;
 
@@ -62,10 +99,7 @@ const Login = () => {
 
         <form onSubmit={handleSubmit}>
           <div className="my-8">
-            <label
-              className="text-gray-600 block text-lg font-bold"
-              htmlFor="email"
-            >
+            <label className="text-gray-600 block text-lg font-bold" htmlFor="email">
               Email
             </label>
             <div className="relative">
@@ -80,16 +114,13 @@ const Login = () => {
               />
 
               <div className="absolute inset-y-0 left-0 flex mt-3 items-center px-2 text-gray-600 text-xl">
-                <AiFillMail/>
+                <AiFillMail />
               </div>
             </div>
           </div>
 
           <div className="my-8">
-            <label
-              className="text-gray-600 block text-lg font-bold"
-              htmlFor="password"
-            >
+            <label className="text-gray-600 block text-lg font-bold" htmlFor="password">
               Password
             </label>
             <div className="relative">
@@ -103,20 +134,14 @@ const Login = () => {
               />
 
               <div className="absolute inset-y-0 left-0 flex mt-3 items-center px-2 text-gray-600 text-xl">
-                <AiFillLock/>
+                <AiFillLock />
               </div>
 
               <div className="absolute inset-y-0 right-0 flex mt-3 items-center px-4 text-gray-600 text-xl">
                 {isPasswordVisible ? (
-                  <AiOutlineEye
-                    onClick={togglePasswordVisibility}
-                    className="cursor-pointer"
-                  />
+                  <AiOutlineEye onClick={togglePasswordVisibility} className="cursor-pointer" />
                 ) : (
-                  <AiOutlineEyeInvisible
-                    onClick={togglePasswordVisibility}
-                    className="cursor-pointer"
-                  />
+                  <AiOutlineEyeInvisible onClick={togglePasswordVisibility} className="cursor-pointer" />
                 )}
               </div>
             </div>
